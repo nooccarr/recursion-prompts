eaf5eb5c4bc86129fc0155c411cab24d2cecd38d
Soohyun Lim <d2axasap@gmail.com>
2020-09-17 20:26:43 -0400

    Make palindrome function in recursion.js pass specs for corresponding test suite in part1.js

--- src/recursion.js
+++ src/recursion.js
@@ -144,6 +144,19 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length === 0) {
+    return true;
+  } else if (string.length === 1) {
+    return true;
+  }
+
+  string = string.toLowerCase();
+
+  if (string[0] === string[string.length - 1]) {
+    return palindrome(string.slice(1, string.length - 1));
+  } else {
+    return false;
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
