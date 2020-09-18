7d1a9eeca02bd8c1ecfadcb2faebfc833d214adb
Soohyun Lim <d2axasap@gmail.com>
2020-09-18 08:04:51 -0400

    Make compare str function and create array function in recursion.js to pass test suite in part1.js

--- src/recursion.js
+++ src/recursion.js
@@ -272,12 +272,27 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
 
+  if (str1[0] === str2[0]) {
+    return compareStr(str1.slice(1), str2.slice(1));
+  } else {
+    return false;
+  }
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  if (str.length === 0) {
+    return [];
+  } else if (str.length === 1) {
+    return [str];
+  }
+
+  return [str[0]].concat(createArray(str.slice(1)));
 };
 
 // 17. Reverse the order of an array
