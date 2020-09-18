f1b22c57107afa81ef5d83d672a7ea850a6f0e18
Soohyun Lim <d2axasap@gmail.com>
2020-09-18 10:58:55 -0400

    Make count occurrence in recursion.js to pass test suite in part1.js

--- src/recursion.js
+++ src/recursion.js
@@ -346,6 +346,15 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (array[0] === value) {
+    return 1 + countOccurrence(array.slice(1), value);
+  } else {
+    return countOccurrence(array.slice(1), value);
+  }
 };
 
 // 21. Write a recursive version of map.
