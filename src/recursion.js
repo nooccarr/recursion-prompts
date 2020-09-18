2e0f65869f63552f62563862f51012a09fb48272
Soohyun Lim <d2axasap@gmail.com>
2020-09-18 12:40:31 -0400

    Make capitalize words in recursion.js to pass suites in part1.js

--- src/recursion.js
+++ src/recursion.js
@@ -374,6 +374,10 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
+  // if (Object.keys(obj).length === 0) {
+  //   return 0;
+  // }
+
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
@@ -394,6 +398,17 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+  if (n <= 0) {
+    return null;
+  } else if (n === 1) {
+    return [0, 1];
+  } else if (n === 2) {
+    return [0, 1, 1];
+  }
+
+  // return (fibonacci(n - 1) + fibonacci(n - 2));
+
+
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
@@ -408,6 +423,13 @@ var nthFibo = function(n) {
 // var words = ['i', 'am', 'learning', 'recursion'];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
 var capitalizeWords = function(array) {
+  if (array.length === 0) {
+    return [];
+  } else if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+
+  return [array.shift().toUpperCase()].concat(capitalizeWords(array.slice()));
 };
 
 // 28. Given an array of strings, capitalize the first letter of each index.
