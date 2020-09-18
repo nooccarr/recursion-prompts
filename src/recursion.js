217f97ff582f07f32579164c28f4511fc26e3394
Soohyun Lim <d2axasap@gmail.com>
2020-09-17 22:00:04 -0400

    Make multiply function in recursion.js to pass its test suite in part1.js

--- src/recursion.js
+++ src/recursion.js
@@ -211,6 +211,17 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  } else if (x < 0 && y < 0) {
+    return multiply(-x, -y);
+  } else if (x < 0) {
+    return -multiply(-x, y);
+  } else if (y < 0) {
+    return -multiply(x, -y);
+  }
+
+  return x + multiply(x, y - 1);
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
